3dd5e1a9494dd8037d3e346dbb76eee13d9055a3
Chris Charles <w0481147@nscc.ca>
2024-01-24 22:05:42 -0400

    Completed Module 4 section 4

---
+++ Module4/Section4/Survey/Marketing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Survey
+{
+    internal class Marketing
+    {
+        public void Finished()
+        {
+            Program.Posted += HasPosted;
+        }
+        void HasPosted()
+        {
+            Console.WriteLine("Thank you for completing the survey. You are now subscribed to ten of our newsletters.");
+        }
+    }
+}
